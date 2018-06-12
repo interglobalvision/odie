9cbdd35af253cd5adb6d87858c3e9be83233b74f
mikeyrayvon <mikey@interglobal.vision>
2018-06-11 19:12:12 -0500

    fix welcomeHeader margin

--- src/components/WelcomeHeader.jsx
+++ src/components/WelcomeHeader.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const WelcomeHeader = () => (
   <header className='grid-row'>
-    <div className='grid-item item-s-12 text-align-center padding-top-basic margin-bottom-extra'>
+    <div className='grid-item item-s-12 text-align-center padding-top-basic margin-bottom-large'>
       Odie makes a webpage with the contents of a google doc
     </div>
   </header>
