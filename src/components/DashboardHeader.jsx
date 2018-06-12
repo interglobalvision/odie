efd992c9b5897b3fab557d633c2cf35f8194accc
mikeyrayvon <mikey@interglobal.vision>
2018-06-12 11:40:03 -0500

    fix header margin

--- src/components/DashboardHeader.jsx
+++ src/components/DashboardHeader.jsx
@@ -3,7 +3,7 @@ import DashboardNav from './DashboardNav';
 import { Link } from 'react-router-dom';
 
 const DashboardHeader = () => (
-  <header className='container grid-row justify-between padding-top-basic margin-bottom-extra'>
+  <header className='container grid-row justify-between padding-top-small margin-bottom-large'>
     <div className='grid-item item-s-3'>
       <h1 className='font-size-large'><Link to='/'>Odie</Link></h1>
     </div>
