ecb4e16392ce0a7929a45245ce779d4700bf2e6e
mikeyrayvon <mikey@interglobal.vision>
2018-06-15 15:57:43 -0500

    add api request

--- src/components/TwitterWidget.jsx
+++ src/components/TwitterWidget.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import axios from 'axios';
 
-const TwitterWidget = () => (
-  <a id="twitter-widget-holder" href="https://twitter.com/odiedotus">
-    <h3 className='u-inline-block'><span className="font-bold">Odie</span> @<span id='twitter-handle'>odiedotus</span></h3> • <span id='tweet-from-now'>1d</span>
-    <div id="tweet-content" className="margin-top-micro">
-      Some recent tweet
-    </div>
-  </a>
-)
+const TwitterWidget = () => {
+  axios.get('http://interglobal.vision/odie-php-apis/twitter.php', {
+    mode: 'no-cors',
+  }).then((response) => {
+    console.log(response);
+  }).catch(function (error) {
+    console.error(error);
+  });
+
+  return (
+    <a id="twitter-widget-holder" href="https://twitter.com/odiedotus">
+      <h3 className='u-inline-block'><span className="font-bold">Odie</span> @<span id='twitter-handle'>odiedotus</span></h3> • <span id='tweet-from-now'>1d</span>
+      <div id="tweet-content" className="margin-top-micro">
+        Some recent tweet
+      </div>
+    </a>
+  );
+}
 
 export default TwitterWidget;
