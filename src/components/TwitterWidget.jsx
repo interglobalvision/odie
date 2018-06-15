a531bcd5b0aa9bae730d1574b900031f469dc9a6
mikeyrayvon <mikey@interglobal.vision>
2018-06-15 17:09:20 -0500

    twitter only sends first image

--- src/components/TwitterWidget.jsx
+++ src/components/TwitterWidget.jsx
@@ -41,14 +41,27 @@ export default class TwitterWidget extends Component {
     if (!isLoading && tweet !== null) {
       const fromNow = moment(tweet.created_at).fromNow();
       const tweetUrl = tweet.entities.urls[0] || null;
+      const tweetMedia = tweet.entities.media[0] || null;
 
       return (
         <div id="twitter-widget-holder">
           <h3 className='u-inline-block'><a href="https://twitter.com/odiedotus"><span className="font-bold">{tweet.user.name}</span> @<span id='twitter-handle'>{tweet.user.screen_name}</span></a></h3> • <span id='tweet-from-now'>{fromNow}</span>
           <div id="tweet-content" className="margin-top-micro">
             { tweetUrl !== null
-              ? <a href={tweetUrl.url} className='link-underline'>{tweet.text}</a>
-              : <span>{tweet.text}</span>
+              ? <div id='tweet-text'>
+                  <a href={tweetUrl.url} className='link-underline'>{tweet.text}</a>
+                </div>
+              : <div id='tweet-text'>
+                  <span>{tweet.text}</span>
+                </div>
+            }
+            { tweetMedia !== null
+              ? <div id='tweet-media' className='margin-top-tiny'>
+                  <a href={tweetMedia.media_url}>
+                    <img src={tweetMedia.media_url} className='twitter-media' />
+                  </a>
+                </div>
+              : null
             }
           </div>
         </div>
