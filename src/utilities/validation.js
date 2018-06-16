e10c6b75bd7dc53d8e96f64f6f2919607f5454a7
mikeyrayvon <mikey@interglobal.vision>
2018-06-16 15:11:14 -0500

    add stripScripts utility function

--- src/utilities/validation.js
+++ src/utilities/validation.js
@@ -10,3 +10,14 @@ export const unescapeHtml = (escapedStr) => {
   const child = div.childNodes[0];
   return child ? child.nodeValue : '';
 }
+
+export const stripScripts = (dangerousInput) => {
+  let div = document.createElement('div');
+  div.innerHTML = dangerousInput;
+  let scripts = div.getElementsByTagName('script');
+  let i = scripts.length;
+  while (i--) {
+    scripts[i].parentNode.removeChild(scripts[i]);
+  }
+  return div.innerHTML; 
+}
