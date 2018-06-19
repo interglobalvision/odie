9bcfccf6ca31795db6f9b600d9da73ff95f3c82e
mikeyrayvon <mikey@interglobal.vision>
2018-06-19 17:43:38 -0500

    add forgot password form compoenent

---
+++ src/components/ForgotPassword.jsx
@@ -0,0 +1,155 @@
+import React, { Component } from 'react';
+import { firebaseConnect } from 'react-redux-firebase';
+import { Link } from 'react-router-dom';
+import AsciiOdie from './AsciiOdie'
+import axios from 'axios';
+import { validateEmail, validatePassword } from '../utilities/validation';
+import { CloudFunctionsUrl } from '../utilities/constants.js';
+import { withRouter } from 'react-router-dom';
+
+export class ForgotPasswordForm extends Component {
+  state = {
+    email: '',
+    password: '',
+    error: {
+      message: '',
+    },
+    isLoading: false,
+  }
+
+  constructor(props) {
+    super(props);
+
+    // Bind
+    this.createUser = this.createUser.bind(this);
+    this.validateForm = this.validateForm.bind(this);
+  }
+
+  componentDidMount() {
+    this.email.focus()
+  }
+
+  createUser() {
+    const { email, password } = this.state;
+
+    // Create user function url
+    const createUserFunction = CloudFunctionsUrl + '/createUser';
+
+    this.setState({ isLoading: true });
+
+    if (this.validateForm()) {
+      // Call create user function
+      this.props.firebase.createUser(
+        { email, password }, // auth user
+        { email }  // user profile
+      ).then(() => {
+
+        // Redirect to dashboard OdieList
+        this.props.history.push('/');
+
+      }).catch(error => {
+
+        this.setState({
+          isLoading: false,
+          error: {
+            message: 'An error has occurred.'
+          }
+        })
+
+        // Error handling
+        if (error) {
+          console.error(error);
+        }
+
+      });
+
+    } else {
+
+      // Invalid form
+      this.setState({ isLoading: false })
+    }
+  }
+
+  validateForm() {
+    const { email, password, passwordConfirm } = this.state;
+
+    if (!validateEmail(email)) {
+      this.setState({
+        error: {
+          message: 'Your email is invalid.',
+        }
+      })
+      return false
+    } else if (!validatePassword(password)) {
+      this.setState({
+        error: {
+          message: 'Your password is invalid.',
+        }
+      })
+      return false
+    } else if (password !== passwordConfirm) {
+      this.setState({
+        error: {
+          message: 'Your password doesn\'t match.',
+        }
+      })
+      return false
+    } else {
+      return true
+    }
+  }
+
+  render() {
+    return (
+      <section className='grid-row justify-center'>
+        <header className='grid-item item-s-12 margin-bottom-tiny grid-row no-gutter justify-center'>
+          <h2 className='grid-item item-s-12 item-l-8'>Forgotten Password</h2>
+        </header>
+
+        <form onSubmit={event => event.preventDefault()} className='grid-item item-s-12 item-m-6 item-l-4 no-gutter'>
+          <div className='grid-row'>
+            <div className='grid-item item-s-12 margin-bottom-small'>
+              <div className='border-top'></div>
+            </div>
+
+            <div className='grid-item item-s-12 margin-bottom-tiny'>
+              <input
+                id='email'
+                ref={ ref => this.email = ref}
+                type='text'
+                placeholder='email'
+                className='margin-bottom-micro'
+                onChange={event => this.setState({ email: event.target.value })}
+              />
+              <label htmlFor='email' className='font-size-small'>
+                <div>Enter the email address associated with your account.</div>
+              </label>
+            </div>
+          </div>
+
+          <div className='grid-row margin-bottom-basic justify-end align-items-center'>
+            { this.state.error.message !== ''
+              ? <div className='grid-item font-size-small color-error item-s-7'>{this.state.error.message}</div>
+              : null
+            }
+            <div className='grid-item item-s-5 text-align-center'>
+              <button className='button-link-style font-size-large' onClick={() => this.createUser()}>
+                Retrieve
+              </button>
+            </div>
+          </div>
+        </form>
+
+        <div className='grid-item grid-item item-s-12 item-m-6 item-l-4 align-self-start'>
+          <div className='text-align-center'>
+            <div className='code-holder padding-top-small padding-bottom-small'>
+              <AsciiOdie />
+            </div>
+          </div>
+        </div>
+      </section>
+    )
+  }
+}
+
+export default firebaseConnect()(withRouter(ForgotPasswordForm));
